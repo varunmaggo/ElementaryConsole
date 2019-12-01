7fb9dc9452a23e644e72c8fb54ddc89ad9ec1002
Varun Maggo <varun.maggo@gmail.com>
2019-12-01 20:15:51 +0530

    initial commit for Reverse String

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ElementaryConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ReverseString("OmegaEpisode");
+
+            Console.ReadLine();
+        }
+
+        static void ReverseString(string myString)
+        {
+            char[] charArray = myString.ToCharArray();
+
+            for (int i = 0, j = myString.Length -1; i < j; i++, j--)
+            {
+                charArray[i] = myString[j];
+                charArray[j] = myString[i];
+
+            }
+
+            string myNewReversedString = new string(charArray);
+
+
+            Console.WriteLine(myNewReversedString);
+        }
+    }
+}
